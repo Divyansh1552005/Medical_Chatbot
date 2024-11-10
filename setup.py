e706d7fb1bf7727435ea898024acf5ac661ea26b
Divyansh1552005 <143210787+Divyansh1552005@users.noreply.github.com>
2024-11-10 15:07:56 +0530

    Folder_structure_added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Project',
+    version= '0.0.0',
+    author= 'Divyansh Sharma',
+    author_email= 'officialdslc1552005@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
